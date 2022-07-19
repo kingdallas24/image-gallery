9a206cd7691ad541e34328be28399c2497a60f7e
Miles Biringanine <milesgabriel23@gmail.com>
2022-07-19 13:37:03 -0500

    Update pictures

--- app.js
+++ app.js
@@ -1,48 +1,28 @@
 const gallery = document.querySelector(".gallery");
 const overlay = document.querySelector(".overlay");
-const overlayImage = overlay.querySelector("img");
-const overlayClose = overlay.querySelector(".close");
+const imageOverlay = overlay.querySelector("img");
+const closerOverlay = overlay.querySelector(".close");
 
-// Create function to generate the HTML for the image
+// Create function to generate HTML
 
 function generateHTML([h, v]) {
   return `
-<div class = "item h${h} v${v}">
-<img src="./images/${randomNumber(7)}.png">
-<div class = "item-overlay">
-<button class ="viewBtn">View --></button>
-</div>
-</div>
-`;
+  <div class = "item h${h} v${v}">
+    <img src = "images/${randomNumber(10)}.jpg">
+    <div class= "item-overlay">
+  <button>View</button>
+    </div>
+  </div>
+  `;
 }
 
-// Create a function to generate a random number for span width and height as well as image #
+// create function to generate random number for item span value and image number
 
 function randomNumber(limit) {
   return Math.floor(Math.random() * limit + 1);
 }
 
-// Grab and display clicked image
-
-// overlayImage.src = src;
-// overlay.classList.add("open");
-
-function handleClick(e) {
-  const src = e.currentTarget.querySelector("img").src;
-  console.log(src);
-  overlayImage.src = src;
-  overlay.classList.add("open");
-}
-
-function close() {
-  overlay.classList.remove("open");
-}
-
-overlayClose.addEventListener("click", close);
-
-// Create a function for when user clicks image
-
-// Create an array with 50 spots that will generate the random span value
+// Create an array of arrays of span values (vertical and horizontal )
 
 const digits = Array.from({ length: 50 }, () => [
   randomNumber(4),
@@ -56,23 +36,36 @@ const digits = Array.from({ length: 50 }, () => [
   [1, 1],
   [1, 1],
   [1, 1],
-  [1, 1],
-  [1, 1],
-  [1, 1],
-  [1, 1],
-  [1, 1],
-  [1, 1],
-  [1, 1],
 ]);
-console.log(digits);
-// Use map to run the generate html function on each item in the digits array
+
+// Connect array and generateHTML() using map to fill in arguements for generateHTML, then covert HTML to string using join method
 
 const html = digits.map(generateHTML).join("");
 
-// Take the gallery and set the innerHTML to the html generated above
+// Fill gallery with random images by setting innerHTML to the string created above
 
 gallery.innerHTML = html;
+
+// Create a open function to be activated when item is clicked by grabbing the currentTarget's img source. Set the overlayed image source to the currentTarget image source. Add open class to display item in the center of the page
+
+function handleClick(e) {
+  const src = e.currentTarget.querySelector("img").src;
+  imageOverlay.src = src;
+  overlay.classList.add("open");
+}
+
+// Create close function that will remove the open class to remove image from page center
+
+function close() {
+  overlay.classList.remove("open");
+}
+
+// Create an eventListener for when item is clicked and run the open function
 
 const items = document.querySelectorAll(".item");
 
 items.forEach((item) => item.addEventListener("click", handleClick));
+
+// Create an eventlistener for when close button is clicked and run the close function
+
+closerOverlay.addEventListener("click", close);
